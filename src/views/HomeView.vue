3751060eccd9aab756aa30792e7686079490235e
Bryant Acero <imbryantacero@gmail.com>
2025-06-09 21:06:53 +0200

    Fix

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,24 +5,15 @@ import TheMonth from '@/components/TheMonth.vue'
 import TheFilters from '@/components/TheFilters.vue'
 import TheReport from '@/components/TheReport.vue'
 import TheTotals from '@/components/TheTotals.vue'
-<<<<<<< HEAD
 import LanguageSelector from '@/components/LanguageSelector.vue'
-=======
->>>>>>> 779e4dda31247d2c2986636b908037a96c071268
 </script>
 
 <template>
   <main class="p-4">
-<<<<<<< HEAD
     <div class="flex justify-between items-center mb-8">
-      <h1 class="text-2xl font-bold text-gray-900">Travel Expense Calculator</h1>
-      <LanguageSelector />
-    </div>
-=======
->>>>>>> 779e4dda31247d2c2986636b908037a96c071268
-    <div class="mb-8">
       <h1 class="text-5xl font-bold text-blue-dark">Monthly Travel Expenses</h1>
-    </div>
+      <LanguageSelector />
+    </div>   
 
     <div class="flex gap-2">
       <div class="bg-white p-6 w-4/6 rounded-xl flex flex-col gap-4">
